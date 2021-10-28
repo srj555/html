3aa666580041d34c57f67df7a1800d5ad2983e72
srj555 <srj555@gmail.com>
2021-10-28 15:54:38 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,5 +6,11 @@
 <meta property="al:android:url" content="spring://launch" />
 <meta property="al:android:app_name" content="Spring" />
 <meta property="al:android:package" content="com.scb.healtheco.alpha" />
+<meta property="al:ios:url" content="applinks://docs" />
+<meta property="al:ios:app_store_id" content="12345" />
+<meta property="al:ios:app_name" content="App Links" />
+<meta property="al:android:url" content="applinks://docs" />
+<meta property="al:android:app_name" content="App Links" />
+<meta property="al:android:package" content="org.applinks" />
 </head>
 </html>
