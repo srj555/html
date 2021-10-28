8443a11d1ce8e8a2d88a664a553bd1a4a0be46ec
srj555 <srj555@gmail.com>
2021-10-28 16:39:10 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <html>
 <head>
 <meta property="al:ios:url" content="spring://launch" />
+<meta property="al:ios:app_store_id" content="12345" />
+<meta property="al:ios:app_name" content="Spring" />
 <meta property="al:android:url" content="spring://launch" />
+<meta property="al:android:app_name" content="Spring" />
+<meta property="al:android:package" content="com.scb.healtheco.alpha" />
 </head>
 </html>
